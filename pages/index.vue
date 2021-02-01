a54954d59f639c4954f9639b533cc0993a9fea00
m-sho1217 <mtytsy15301217@gmail.com>
2021-02-01 10:00:20 +0900

    あああ

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="HOME" class="wrap">
     <Header></Header>
+    <div class="uk-inline">
+      <div class="search">
+        <form @submit="searchItems">
+          <div class="float-right uk-margin" uk-margin>
+              <div uk-form-custom="target: true">
+                <input v-model="search" class="uk-search-input" type="search" placeholder="">
+              </div>
+              <button class="uk-button uk-button-default">Submit</button>
+          </div>
+        </form>
+      </div>
+      <p>{{ search }}</p>
+    </div>
     <div class="title">
       <h1>商品一覧</h1>
     </div>
     <div class="container uk-child-width-1-2@s uk-grid-match" uk-grid>
-      <div class="item-container btn uk-card uk-card-default uk-card-hover uk-card-body" v-for="item_id in Object.keys(items)" :key="item_id">
-        <nuxt-link :to="'/detail/'+ item_id">
-          <img :src="items[item_id].image_url" alt="商品画像">
-          <p class="name">{{ items[item_id].name }}</p>
-          <p class="price">¥ {{ items[item_id].price }}</p>
-        </nuxt-link>
+      <div v-for="item_id in Object.keys(items)" :key="item_id">
+        <div class="item-container btn uk-card uk-card-default uk-card-hover uk-card-body" v-if="items[item_id].name.includes(search)">
+          <nuxt-link :to="'/detail/'+ item_id">
+            <img :src="items[item_id].image_url" alt="商品画像">
+            <p class="name">{{ items[item_id].name }}</p>
+            <p class="price">¥ {{ items[item_id].price }}</p>
+          </nuxt-link>
+        </div>
       </div>
     </div>
   </div>
@@ -25,7 +40,8 @@
   export default {
     data() {
       return {
-        items: {}
+        items: {},
+        search: ""
       }
     },
     created() {
@@ -43,6 +59,23 @@
       addItems(id, item) {
         Vue.set(this.items, id, item);
         console.log(this.items);
+      },
+      searchItems(e) {
+        const _addItems = this.addItems
+        const _clearItems = this.clearItems
+        console.log(this.search)
+        e.preventDefault();
+        db.collection('items').where("name", "==", this.search).get().then(function(querySnapshot) {
+          // _clearItems()
+          console.log(querySnapshot);
+          querySnapshot.forEach(function(doc) {
+            _addItems(doc.id, doc.data());
+            console.log(doc.id, " => ", doc.data());
+          });
+        });
+      },
+      clearItems() {
+        this.items = {}
       }
     }
   }
